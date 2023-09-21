8edc4ef09f77c478dc23cd59b84d35a3f499a6ca
kimmengs <129708168+kimmengs@users.noreply.github.com>
2023-09-21 10:11:53 +0700

    Add lesson 2

---
+++ lesson-2-import-export-module/index.js
@@ -0,0 +1,11 @@
+// main.js
+
+// Importing the mathOperations module
+const mathOperations = require('./mathOperation');
+
+// Using the imported functions
+const result1 = mathOperations.add(5, 3);
+const result2 = mathOperations.subtract(10, 4);
+
+console.log('Addition Result:', result1);
+console.log('Subtraction Result:', result2);
---
+++ lesson-2-import-export-module/mathOperation.js
@@ -0,0 +1,17 @@
+// mathOperations.js
+
+// Function to add two numbers
+function add(a, b) {
+  return a + b;
+}
+
+// Function to subtract two numbers
+function subtract(a, b) {
+  return a - b;
+}
+
+// Exporting functions as properties of an object
+module.exports = {
+  add,
+  subtract,
+};
