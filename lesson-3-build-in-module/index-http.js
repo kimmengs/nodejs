9a9e6f30393e1d8b90f16e15b85a6f5e3c51c58c
kimmengs <129708168+kimmengs@users.noreply.github.com>
2023-09-21 10:15:22 +0700

    add lesson 3 build-in module

---
+++ lesson-3-build-in-module/index-http.js
@@ -0,0 +1,13 @@
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Helo, World \n');
+});
+
+const port = 3000;
+
+server.listen(port, () => {
+    console.log(`Server listing on port ${port}`);
+});
---
+++ lesson-3-build-in-module/index-os.js
@@ -0,0 +1,7 @@
+const os = require('os');
+
+console.log('Operation System Type: ', os.type());
+
+console.log('CPU Architecture:', os.arch());
+
+console.log('System Uptime (second):', os.uptime());
